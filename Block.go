5dff4e8c47c7b01654b6def42be5a5009950ef7c
QuantumSpark <jamespark1234@hotmail.com>
2018-02-02 15:42:29 -0800

    added basic functions

---
+++ Block.go
@@ -0,0 +1,24 @@
+package main
+
+
+type Block struct {
+	CurrentHash string
+	PreviousHash string
+	OPS []Operation
+
+}
+
+
+type Operation struct {
+	Command string
+	Shapetype string
+	ShapeSvgString string
+	Fill string
+	Stroke string
+}
+
+
+
+
+
+
---
+++ MinerAPI.go
@@ -0,0 +1,15 @@
+package main
+
+
+type Miner interface {
+
+	Register(address string, publicKey string) (string, error)
+
+	GetNodes(publicKey string) ([]string, error)
+
+	HeartBeat(publicKey string) (error)
+
+	Mine(currentBlock Block, newOperation Operation) (string, error)
+
+}
+
