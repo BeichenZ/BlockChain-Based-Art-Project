3982efe581054dc51090ab1cc5ad9a1f75951300
Howard-Zhou <howard12345678987654321@hotmail.com>
2018-02-04 23:59:27 -0800

    Server and test script included, absolutely nothing is working

--- art-app.go
+++ art-app.go
@@ -11,51 +11,56 @@ package main
 
 // Expects blockartlib.go to be in the ./blockartlib/ dir, relative to
 // this art-app.go file
+import "./blockartlib"
 
-// func main() {
-	// minerAddr := "127.0.0.1:8080"
-	// privKey := ecdsa.PrivateKey{} // TODO: use crypto/ecdsa to read pub/priv keys from a file argument.
-	//
-	// // Open a canvas.
-	// canvas, settings, err := blockartlib.OpenCanvas(minerAddr, privKey)
-	// if checkError(err) != nil {
-	// 	return
-	// }
-	//
-	// validateNum := 2
-	//
-	// // Add a line.
-	// shapeHash, blockHash, ink, err := canvas.AddShape(validateNum, blockartlib.PATH, "M 0 0 L 0 5", "transparent", "red")
-	// if checkError(err) != nil {
-	// 	return
-	// }
-	//
-	// // Add another line.
-	// shapeHash2, blockHash2, ink2, err := canvas.AddShape(validateNum, blockartlib.PATH, "M 0 0 L 5 0", "transparent", "blue")
-	// if checkError(err) != nil {
-	// 	return
-	// }
-	//
-	// // Delete the first line.
-	// ink3, err := canvas.DeleteShape(validateNum, shapeHash)
-	// if checkError(err) != nil {
-	// 	return
-	// }
-	//
-	// // assert ink3 > ink2
-	//
-	// // Close the canvas.
-	// ink4, err := canvas.CloseCanvas()
-	// if checkError(err) != nil {
-	// 	return
-	// }
-// }
+import "fmt"
+import "os"
+import "crypto/ecdsa"
+
+func main() {
+	minerAddr := "127.0.0.1:8080"
+	privKey := // TODO: use crypto/ecdsa to read pub/priv keys from a file argument.
+
+	// Open a canvas.
+	canvas, settings, err := blockartlib.OpenCanvas(minerAddr, privKey)
+	if checkError(err) != nil {
+		return
+	}
+
+    validateNum := 2
+
+	// Add a line.
+	shapeHash, blockHash, ink, err := canvas.AddShape(validateNum, blockartlib.PATH, "M 0 0 L 0 5", "transparent", "red")
+	if checkError(err) != nil {
+		return
+	}
+
+	// Add another line.
+	shapeHash2, blockHash2, ink2, err := canvas.AddShape(validateNum, blockartlib.PATH, "M 0 0 L 5 0", "transparent", "blue")
+	if checkError(err) != nil {
+		return
+	}
+
+	// Delete the first line.
+	ink3, err := canvas.DeleteShape(validateNum, shapeHash)
+	if checkError(err) != nil {
+		return
+	}
+
+	// assert ink3 > ink2
+
+	// Close the canvas.
+	ink4, err := canvas.CloseCanvas()
+	if checkError(err) != nil {
+		return
+	}
+}
 
 // If error is non-nil, print it out and return it.
-// func checkError(err error) error {
-// 	if err != nil {
-// 		fmt.Fprintf(os.Stderr, "Error ", err.Error())
-// 		return err
-// 	}
-// 	return nil
-// }
+func checkError(err error) error {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error ", err.Error())
+		return err
+	}
+	return nil
+}
